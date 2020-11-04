1f17747d5b1b2738abb26963b0cfecbbe917f395
newmentos <newmentos@ascii>
2020-11-04 14:36:46 +0300

    Добавлен файл настроек sxhkdrc

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,10 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
 # If all your bars have ipc enabled, you can also use 
 # polybar-msg cmd quit
 
-polybar bar1 &
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-echo "Polybar started ..."
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==2{print $1}') MONITOR=$m polybar --reload mainbar-bspwm &
+done
+
+echo "Bars launched..."
